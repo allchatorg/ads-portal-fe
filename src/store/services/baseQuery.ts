62080ca783697b0146f95743cdb7330a7e5272e3
markok32000 <marko.angelovski2000@yahoo.com>
2025-12-24 16:49:41 +0100

    Update `baseQuery` to fallback to default backend URL if `NEXT_PUBLIC_BACKEND_URL` is not set

--- src/store/services/baseQuery.ts
+++ src/store/services/baseQuery.ts
@@ -1,7 +1,12 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+const backendUrl =
+    process.env.NEXT_PUBLIC_BACKEND_URL && process.env.NEXT_PUBLIC_BACKEND_URL.length > 0
+        ? process.env.NEXT_PUBLIC_BACKEND_URL
+        : 'http://nocw0kowww8ko8swk048c8w4.23.137.253.173.sslip.io';
+
 export const baseQuery = fetchBaseQuery({
-    baseUrl: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api`,
+    baseUrl: `${backendUrl}/api`,
     prepareHeaders: (headers, { getState }) => {
         const token = (getState() as { auth: { token: string | null } }).auth.token;
 
